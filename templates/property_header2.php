327305aefec6bc10af65aabefc21a987dff4a2c7
iriepixel <jev@iriepixel.com>
2018-02-11 01:51:45 +0100

    book now button renamed + location hover state changed

--- templates/property_header2.php
+++ templates/property_header2.php
@@ -148,7 +148,7 @@ $property_area      =   get_the_term_list($post->ID, 'property_area', '', ', ', 
         ?>
          " id="booking_form_request">
         <div id="booking_form_request_mess"></div>
-            <h3 ><?php esc_html_e('Book Now','wpestate');?></h3>
+            <h3 ><?php esc_html_e('Request to Book','wpestate');?></h3>
                 <div>
                     <div class="has_calendar calendar_icon">
                         <input type="text" id="start_date" placeholder="<?php esc_html_e('Check in','wpestate'); ?>"  class="form-control calendar_icon" size="40" name="start_date" 
@@ -211,7 +211,7 @@ $property_area      =   get_the_term_list($post->ID, 'property_area', '', ', ', 
                         if($instant_booking ==1){ ?>
                             <div id="submit_booking_front_instant_wrap"><input type="submit" id="submit_booking_front_instant" data-maxguest="<?php echo $max_guest; ?>" data-overload="<?php echo $overload_guest;?>" data-guestfromone="<?php echo $price_per_guest_from_one; ?>"  class="wpb_btn-info wpb_btn-small wpestate_vc_button  vc_button" value=" <?php esc_html_e('Instant Booking','wpestate');?>" /></div>
                         <?php }else{?>   
-                            <input type="submit" id="submit_booking_front" data-maxguest="<?php echo $max_guest; ?>" data-overload="<?php echo $overload_guest;?>" data-guestfromone="<?php echo $price_per_guest_from_one; ?>"  class="wpb_btn-info wpb_btn-small wpestate_vc_button  vc_button" value="<?php esc_html_e('Book Now','wpestate');?>" />
+                            <input type="submit" id="submit_booking_front" data-maxguest="<?php echo $max_guest; ?>" data-overload="<?php echo $overload_guest;?>" data-guestfromone="<?php echo $price_per_guest_from_one; ?>"  class="wpb_btn-info wpb_btn-small wpestate_vc_button  vc_button" value="<?php esc_html_e('Request to Book','wpestate');?>" />
                         <?php }?>
                             
                         <?php wp_nonce_field( 'booking_ajax_nonce', 'security-register-booking_front' );?>
